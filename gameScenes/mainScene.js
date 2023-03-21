f799e74411275270ccc1f36ffcd7e8cba5b67033
Diogo Pinheiro <120480585+diogoPinheiro11@users.noreply.github.com>
2023-03-21 11:36:26 +0000

    added sprites/camera limitation/mario movement code

--- gameScenes/mainScene.js
+++ gameScenes/mainScene.js
@@ -3,14 +3,14 @@ let game;
 const gameOptions = {
     playerGravity: 1100,
     playerSpeed: 150,
-    playerJump: 400
+    playerJump: 350
 };
 
 window.onload = function() {
     const gameConfig = {
         type: Phaser.CANVAS,
-        width: 312,
-        height: 312,
+        width: 256,
+        height: 256,
         backgroundColor: '6bccef',
         physics: {
             default: "arcade",
@@ -30,10 +30,13 @@ class PreloadGame extends Phaser.Scene {
         super("PreloadGame");
     }
     preload() {
+
         this.load.tilemapTiledJSON("level", "assets/level.json");
         this.load.image("tile", "assets/tile.png");
         this.load.image("mario", "assets/mario.png");
+        this.load.image("logo", "assets/superMarioLogo.png")
     }
+
     create() {
         this.scene.start("PlayGame");
     }
@@ -42,12 +45,18 @@ class PreloadGame extends Phaser.Scene {
 var jumpButton;
 var runButton;
 var result;
+var shiftCooldown = 10;
+let addCollider = true;
+
 
 class PlayGame extends Phaser.Scene {
     constructor() {
         super("PlayGame");
     }
     create() {
+
+        var logo = this.add.image(500, 400, "logo");
+
         // cria um objeto de teclas de seta
         this.cursors = this.input.keyboard.createCursorKeys();
     
@@ -62,5 +71,48 @@ class PlayGame extends Phaser.Scene {
 
         this.layer = this.map.createStaticLayer("layer01", tile);
     
+        // cria o sprite do personagem e as coordenadas 
+        this.mario = this.physics.add.sprite(90, 150, "mario");
+        this.mario.body.velocity.x = 0;
+        this.mario.body.velocity.y = 0;
+        this.mario.body.gravity.y = gameOptions.playerGravity;
+        this.mario.setCollideWorldBounds(false);
+        
+        // codigo para definir a câmera/limites e para seguir o mario
+        this.cameras.main.setBounds(0, 0, 1920, 1440);
+        this.cameras.main.startFollow(this.mario);
+
+        this.physics.add.collider(this.mario, this.layer);
+    }
+
+    update(){
+        
+        shiftCooldown -= 1;
+
+        // codigo para movimentar o mario | falta as animações
+        if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A).isDown) {
+            this.mario.body.velocity.x = -gameOptions.playerSpeed;
+        } 
+        else if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D).isDown) {
+            this.mario.body.velocity.x = gameOptions.playerSpeed;
+        } 
+        else {
+            this.mario.body.velocity.x = 0;
+        }
+        if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W).isDown && this.mario.body.onFloor()){
+            this.mario.setVelocityY(-gameOptions.playerJump);
+        }
+        if((this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT).isDown)){
+            if(shiftCooldown == 0){
+                if(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A).isDown){
+                    this.mario.body.velocity.x = -gameOptions.playerSpeed * 2;
+                }
+                else{
+                    this.mario.body.velocity.x = gameOptions.playerSpeed * 2;
+                }
+            }
+            
+        }
+        
     }
 }
