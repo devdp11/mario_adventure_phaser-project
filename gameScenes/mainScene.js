149f444afc69c912df100a0d902a085baaa9493b
Diogo Pinheiro <120480585+diogoPinheiro11@users.noreply.github.com>
2023-03-22 14:21:29 +0000

    added mario movement/changed mario spritesheet/score increases when the player gets a coin/commented what the code does

--- gameScenes/mainScene.js
+++ gameScenes/mainScene.js
@@ -30,10 +30,12 @@ class PreloadGame extends Phaser.Scene {
         super("PreloadGame");
     }
     preload() {
-        this.load.image("coin", "assets/coin.png");
+
         this.load.tilemapTiledJSON("level", "assets/level.json");
         this.load.image("tile", "assets/tile.png");
-        this.load.image("mario", "assets/mario.png");
+        this.load.image("coin", "assets/coin.png");
+        this.load.spritesheet('mario', 'assets/mario.png',{ frameWidth: 37, frameHeight: 19 });
+        this.load.image("enemy", "assets/enemy.png");
         this.load.image("logo", "assets/superMarioLogo.png")
     }
 
@@ -42,40 +44,50 @@ class PreloadGame extends Phaser.Scene {
     }
 }
 
+// variaveis
 var coin;
+let scoreText = ""; 
+var score = 0;
+var result;
+
 var jumpButton;
 var runButton;
-var result;
-let addCollider = true;
 
+let addCollider = true;
 
 class PlayGame extends Phaser.Scene {
     constructor() {
         super("PlayGame");
     }
     create() {
+        
         // codigo de implementação de logo no background do mapa
         var logo = this.add.image(500, 400, "logo");
 
         // cria um objeto de teclas de seta
         this.cursors = this.input.keyboard.createCursorKeys();
     
-        // cria o tilemap
+        // codigo de implementação do tilemap e tileset no ecrã
         this.map = this.make.tilemap({ key: "level" });
         const tile = this.map.addTilesetImage("tileset01", "tile");
         
+        // blocos cujo player colide ao pousar
         this.map.setCollisionBetween(16, 17);
         this.map.setCollisionBetween(27, 28);
         this.map.setCollisionBetween(21, 22);
         this.map.setCollision(40);
 
+        // criação de uma variavel layer que quando é chamada vai para o "layer01" do tilemap "level.json"
         this.layer = this.map.createStaticLayer("layer01", tile);
         
-        //codigo teste para implementação da moeda no jogo
+        // codigo de implementação do mario (player) e da moeda coletável, juntamente com as suas coordenadas iniciais
         this.coin = this.physics.add.sprite(100, 100, "coin");
-
-        // cria o sprite do personagem e as coordenadas 
         this.mario = this.physics.add.sprite(90, 150, "mario");
+        
+        // codigo de implementação para o mario colidir ao tocar no "bloco"
+        this.physics.add.collider(this.mario, this.layer);
+        this.physics.add.collider(this.mario, this.coin, getcoin, null, this);
+
 
         this.mario.body.velocity.x = 0;
         this.mario.body.velocity.y = 0;
@@ -83,32 +95,43 @@ class PlayGame extends Phaser.Scene {
         this.mario.setCollideWorldBounds(false);
         
         // codigo para definir a câmera/limites e para seguir o mario
-        this.cameras.main.setBounds(0, 0, 1920, 1440);
+        this.cameras.main.setBounds(0, 0, 960, 720);
         this.cameras.main.startFollow(this.mario);
 
-        this.physics.add.collider(this.mario, this.layer);
+        this.scoreText = this.add.text(25, 25, `score: ${score}`, { fontSize: '16px', fill: '#000' }).setScrollFactor(0);
 
-        this.physics.add.collider(this.mario, this.coin, this.getcoin, null, this);
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('mario', { start: 1, end: 4 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'mario', frame: 6 } ],
+            frameRate: 20
+        });
+        
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('mario', { start: 7, end: 10 }),
+            frameRate: 10,
+            repeat: -1
+        });
 
-       // this.anims.create({
-       //     key: 'left',
-       //     frames: this.anims.generateFrameNumbers('mario', { start: 1, ent: 1}),
-       //     frameRate: 10,
-       //     repeat: -1
-       // });
-
-       // this.anims.create({
-       //     key: 'turn',
-       //     frames: [{key: 'mario', frame: 0 }],
-       //     frameRate: 20,
-       // });
-
-       // this.anims.create({
-       //     key: 'right',
-       //     frames: this.anims.generateFrameNumbers('mario', { start: 1, ent: 1}),
-       //     frameRate: 10,
-       //     repeat: -1
-       // });
+        this.anims.create({
+            key: 'spin',
+            frames: this.anims.generateFrameNumbers('coin', { start: 0, end: 6 }),
+            frameRate: 5,
+            repeat: -1
+        });
+        
+        if (this.coin && this.coin.children) {
+            this.coin.children.iterate(function (child) {
+                child.anims.play('spin', true);
+            });
+        }
 
     }
 
@@ -117,35 +140,38 @@ class PlayGame extends Phaser.Scene {
         // codigo para movimentar o mario | falta as animações
         if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A).isDown) {
             this.mario.body.velocity.x = -gameOptions.playerSpeed;
+            this.mario.anims.play('left', true);
         } 
         else if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D).isDown) {
             this.mario.body.velocity.x = gameOptions.playerSpeed;
+            this.mario.anims.play('right', true);
         } 
         else {
             this.mario.body.velocity.x = 0;
+            this.mario.anims.play('turn');
         }
         if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W).isDown && this.mario.body.onFloor()){
             this.mario.setVelocityY(-gameOptions.playerJump);
         }
         if((this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT).isDown)){
-            if(shiftCooldown == 0){
                 if(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A).isDown){
                     this.mario.body.velocity.x = -gameOptions.playerSpeed * 2;
                 }
                 else{
                     this.mario.body.velocity.x = gameOptions.playerSpeed * 2;
-                }
-            }
-            
+                }     
         }
-
     }
 }
 
+// função para a moeda desaparecer quando o mario colidir com ela
 function getcoin(mario, coin){
     coin.disableBody(true, true);
+    score += 1;
+    this.scoreText.setText('score: ' + score);
 }
 
-function raiseResult(coin, result){
+// função para o score aumentar quando cada moeda é apanhada
+function raiseScore(mario, coin, score){
 
 }
